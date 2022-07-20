2c3873c912e908cff70cc502431e477835ecf907
Mohit Trivedi <108317150+Mohitrivedi@users.noreply.github.com>
2022-07-21 00:00:00 +0530

    Add files via upload

---
+++ day10/style.js
@@ -0,0 +1,11 @@
+function changefont(){
+	document.getElementById("hone").style.fontFamily="newfont";	
+}
+
+function changesize(){
+	document.getElementById("hone").style.fontSize="50px";	
+}
+
+function hide(){
+	document.getElementById("hone").style.display=none;	
+}
